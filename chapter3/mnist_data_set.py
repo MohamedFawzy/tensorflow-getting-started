27a218c76f005e14abc670faa53e7a99a62dc7ea
Mohamed Fawzy <mfawzy22@gmail.com>
2018-02-16 14:16:20 +0200

    plot mnist data

--- chapter3/mnist_data_set.py
+++ chapter3/mnist_data_set.py
@@ -11,3 +11,12 @@ mnist_images = input_data.read_data_sets("MNIST_data/", one_hot=True)
 pixels, real_values = mnist_images.train.next_batch(10)
 
 print "list of values loaded ", real_values
+
+example_to_visualize = 5
+print "element N " + str(example_to_visualize + 1) + " of the list plotted"
+
+image = pixels[example_to_visualize, :]
+image = np.reshape(image, [28, 28])
+plt.imshow(image)
+plt.show()
+
