015a0492c73001177cd9f8f3336d76db58f908d1
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-09 16:21:41 +0200

    finished multi layer code

--- chapter4/MLP.py
+++ chapter4/MLP.py
@@ -38,4 +38,47 @@ bias_layer_2 = tf.Variable(tf.random_normal([n_hidden_2]))
 layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1,w),bias_layer_2))
 #weights output layer
 output = tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
-#biar output layer
+#bias output layer
+bias_output = tf.Variable(tf.random_normal([n_classes]))
+# output layer
+output_layer = tf.matmul(layer_2, output) + bias_output
+# cost function
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output_layer, labels=y))
+# optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+# plot setting
+avg_set = []
+epoch_set = []
+# init variables
+init = tf.global_variables_initializer()
+# Launch graph
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(mnist_images.train.num_examples/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist_images.train.next_batch(batch_size)
+            # Fit training using batch data
+            sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
+            # Compute average loss
+            avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys}) / total_batch
+            # Display logs per epoch step
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost)
+        avg_set.append(avg_cost)
+        epoch_set.append(epoch + 1)
+    print "Training phase finished"
+    plt.plot(epoch_set, avg_set, 'o', label='MLP Training phase')
+    plt.ylabel('cost')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(output_layer, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print "Model Accuracy:", accuracy.eval({x: mnist_images.test.images, y: mnist_images.test.labels})
